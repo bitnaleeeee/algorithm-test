1526adb87fe76ed9475d8fd9069ed48367d63c03
이빛나 Bitna Lee <bitnaleeeee@gmail.com>
2024-02-02 11:38:29 +0900

    [level 0] Title: 홀짝에 따라 다른 값 반환하기, Time: 0.14 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
+++ 프로그래머스/0/181935. 홀짝에 따라 다른 값 반환하기/홀짝에 따라 다른 값 반환하기.js
@@ -1,21 +1,18 @@
 function solution(n) {
     var answer = 0;
-    for (i=1; i<=n; i++){
 
-       if ( n % 2 === 0) {
+    for (i=1; i<=n; i++) {
+       if ( n % 2 === 0 && i) {
          if ( i % 2 === 0) {
-             console.log(i)        
+             answer += (i ** 2)
          }
-
        } 
     
         else {
             if ( i % 2 === 1) {
-                console.log(i)
-            }
-        }
-        
-        
+            answer += i        
+            }            
+        }       
     }
     return answer;
 }
